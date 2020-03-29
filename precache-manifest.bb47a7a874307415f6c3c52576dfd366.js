b459d54c742140c32ac61f806d0af7db41130797
Malikbook <55987446+Malikbook@users.noreply.github.com>
2020-03-29 20:56:34 +0300

    Updates

---
+++ precache-manifest.bb47a7a874307415f6c3c52576dfd366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4a8c60d937685b32783",
+    "url": "/React-CinemaList/static/js/main.85f66b03.chunk.js"
+  },
+  {
+    "revision": "7050c2e316e56932a1fe",
+    "url": "/React-CinemaList/static/js/runtime~main.f4af557f.js"
+  },
+  {
+    "revision": "6fbfbf1477e4bd10bf7c",
+    "url": "/React-CinemaList/static/css/2.90110785.chunk.css"
+  },
+  {
+    "revision": "6fbfbf1477e4bd10bf7c",
+    "url": "/React-CinemaList/static/js/2.341fbce4.chunk.js"
+  },
+  {
+    "revision": "ecf0cdf7bec0555db28cc8edf7ecfd4b",
+    "url": "/React-CinemaList/index.html"
+  }
+];
