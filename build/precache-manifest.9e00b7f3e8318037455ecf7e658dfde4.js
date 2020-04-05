d8969c694a2b256c11ee224afd5ef0af5a7aa8c5
Malikbook <55987446+Malikbook@users.noreply.github.com>
2020-04-05 03:50:52 +0300

    synchronization

---
+++ build/precache-manifest.9e00b7f3e8318037455ecf7e658dfde4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c8c2460220984b171b",
+    "url": "/React-CinemaList/static/js/main.48d8b2f8.chunk.js"
+  },
+  {
+    "revision": "7050c2e316e56932a1fe",
+    "url": "/React-CinemaList/static/js/runtime~main.f4af557f.js"
+  },
+  {
+    "revision": "ea56079bdb9a8fe119b3",
+    "url": "/React-CinemaList/static/css/2.b06bf2bb.chunk.css"
+  },
+  {
+    "revision": "ea56079bdb9a8fe119b3",
+    "url": "/React-CinemaList/static/js/2.e6a24d7e.chunk.js"
+  },
+  {
+    "revision": "2b84b75f903b52631614c697a3bfeb5a",
+    "url": "/React-CinemaList/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-CinemaList/precache-manifest.018f5d08bc635db4044e018907701b84.js"
+  "/React-CinemaList/precache-manifest.9e00b7f3e8318037455ecf7e658dfde4.js"
 );
 
 workbox.clientsClaim();
