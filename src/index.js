cd7418103a1c37e3e28b318bfdd59a7e75652d09
Malikbook <55987446+Malikbook@users.noreply.github.com>
2022-01-31 20:51:42 +0200

    Test commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//Test
+console.log('ok');
+
 ReactDOM.render(<App />, document.getElementById("root"));
